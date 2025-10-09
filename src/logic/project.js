715e355bc5f766dbc11e20c0b2e3df87070f84ed
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-10-09 15:32:40 +0300

    Create project and todos handling modules

---
+++ src/logic/project.js
@@ -0,0 +1,53 @@
+import { createTodo } from "./todo";
+
+function generateId() {
+  return Math.random().toString(36).slice(0, 16);
+}
+
+export function createProject(props) {
+  const id = props.id || generateId();
+  let title = props.title || "";
+  let todos = new Map();
+
+  function getProps() {
+    return {
+      id,
+      title,
+      todos: Array.from(todos.values()).map((todo) => todo.getProps()),
+    };
+  }
+
+  function setProps(props) {
+    if (props.title !== undefined) {
+      title = props.title;
+    }
+    if (props.todos !== undefined) {
+      todos.clear();
+      for (let todo of props.todos) {
+        addTodo(todo);
+      }
+    }
+  }
+
+  function addTodo(todo) {
+    todos.set(todo.getProps().id, todo);
+  }
+
+  function editTodo(props) {
+    if (props.id !== undefined) {
+      todos[props.id].setProps(props);
+    }
+  }
+
+  function removeTodo(id) {
+    return todos.delete(id);
+  }
+
+  return {
+    getProps,
+    setProps,
+    addTodo,
+    editTodo,
+    removeTodo,
+  };
+}
