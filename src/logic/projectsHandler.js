96394ca2de6a4727443343b9b7b9cfe9f17a3691
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-10-29 23:38:26 +0300

    Make todos and projects logic more consistent + extend projectsHandler functionality
    Due to some functions parsing props instead of objects from `create...`, adjustments were made to ensure parsing of objects.
    Furthermore, `projectsHandler` was extended with functions to add, edit and remove todos within projects.

--- src/logic/projectsHandler.js
+++ src/logic/projectsHandler.js
@@ -1,3 +1,4 @@
+import { createTodo } from "./todo";
 import { createProject } from "./project";
 import { localStorageHandler } from "./localStorageHandler";
 
@@ -32,19 +33,56 @@ export const projectsHandler = (function (storageHandler) {
     storageHandler.removeItem(id);
   }
 
+  function addTodoToProject(projectId, todo) {
+    if (projectId && projects.has(projectId)) {
+      projects.get(projectId).addTodo(todo);
+
+      updateInStorage(projects.get(projectId));
+    }
+  }
+
+  function editTodoInProject(projectId, todoProps) {
+    if (projectId && projects.has(projectId)) {
+      projects.get(projectId).editTodo(todoProps);
+
+      updateInStorage(projects.get(projectId));
+    }
+  }
+
+  function removeTodoFromProject(projectId, todoId) {
+    if (projectId && projects.has(projectId)) {
+      projects.get(projectId).removeTodo(todoId);
+
+      updateInStorage(projects.get(projectId));
+    }
+  }
+
   function updateInStorage(project) {
+    const todosArray = Array.from(project.getProps().todos).map((todo) =>
+      todo.getProps()
+    );
+
     storageHandler.populate({
-      [project.getProps().id]: project.getProps(),
+      [project.getProps().id]: {
+        title: project.getProps().title,
+        todos: todosArray,
+      },
     });
   }
 
   for (let [key, value] of Object.entries(storageHandler.getItems())) {
-    addProject(
-      createProject({
-        id: key,
-        ...value,
-      })
-    );
+    const project = createProject({
+      id: key,
+      title: value.title,
+    });
+
+    if (value.todos && Array.isArray(value.todos)) {
+      value.todos.forEach((todoProps) => {
+        project.addTodo(createTodo(todoProps));
+      });
+    }
+
+    addProject(project);
   }
 
   return {
@@ -53,5 +91,8 @@ export const projectsHandler = (function (storageHandler) {
     addProject,
     editProject,
     removeProject,
+    addTodoToProject,
+    editTodoInProject,
+    removeTodoFromProject,
   };
 })(localStorageHandler);
